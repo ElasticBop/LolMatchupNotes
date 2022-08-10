93ecd3a68dd74753f295746d283d31c8b7ca19ee
ElasticBop <53546840+ElasticBop@users.noreply.github.com>
2022-08-10 15:12:40 -0700

    Prepare for deployment
    Built react files and updated server to serve the files

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv").config();
 const {errorHandler} = require("./middleware/errorMiddleware")
 const connectDB = require('./config/db');
 const cors = require("cors")
+const path = require("path")
 
 const port = process.env.PORT || 5000;
 
@@ -17,6 +18,11 @@ app.use(express.urlencoded({extended: false}));
 app.use("/api/schedules", require("./routes/scheduleRoutes") );
 app.use("/api/matchups", require("./routes/matchupRoutes") );
 app.use("/api/users", require("./routes/userRoutes") );
+
+if( process.env.NODE_ENV === "production"){
+	app.use(express.static(path.join(__dirname, "../build")))
+	app.get("*", (req,res) => res.sendFile(path.resolve(__dirname, "../build", "index.html")))
+}
 
 app.use(errorHandler);
 
